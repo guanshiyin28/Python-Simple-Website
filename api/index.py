183a6ad0ebad3ba0f52b7553e04a69a9084f6e4d
guanshiyin28 <guanshiyinnn@gmail.com>
2025-01-22 10:34:51 +0700

    Fix

--- api/index.py
+++ api/index.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('../home.html')
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return render_template('../about.html')
+    return render_template('about.html')
 
 @app.route('/projects')
 def projects():
-    return render_template('../projects.html')
+    return render_template('projects.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('../contact.html')
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
